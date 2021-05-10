04ca774985062eb924fb41fa44439637fabcba37
Lebron Zheng <2207549516@qq.com>
2021-05-11 00:27:32 +0800

    test0.4

--- src/HiGit.java
+++ src/HiGit.java
@@ -3,6 +3,7 @@ public class HiGit {
         System.out.println("HiGit");
         System.out.println("hello");
         System.out.println("to do....");
+        System.out.println("this is book");
 
     }
 }
